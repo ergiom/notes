1f897bf575f31bda1f1dfda890ce9f8fdbd1db1f
ergiom <ergiom1@gmail.com>
2023-01-09 19:45:35 +0100

    added methods to notebook controller

--- src/main/java/com/example/notes/service/NotebookService.java
+++ src/main/java/com/example/notes/service/NotebookService.java
@@ -1,4 +1,14 @@
 package com.example.notes.service;
 
+import com.example.notes.entity.Notebook;
+import com.example.notes.error.NotebookNotFoundException;
+
+import java.util.List;
+
 public interface NotebookService {
+    List<Notebook> getAllNotebooks();
+
+    void saveNotebook(Notebook notebook);
+
+    Notebook getNotebookById(Long id) throws NotebookNotFoundException;
 }
--- src/main/java/com/example/notes/service/NotebookServiceImpl.java
+++ src/main/java/com/example/notes/service/NotebookServiceImpl.java
@@ -1,4 +1,38 @@
 package com.example.notes.service;
 
+import com.example.notes.entity.Notebook;
+import com.example.notes.error.NotebookNotFoundException;
+import com.example.notes.repository.NotebookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class NotebookServiceImpl implements NotebookService {
+
+    @Autowired
+    private NotebookRepository notebookRepository;
+
+    @Override
+    public List<Notebook> getAllNotebooks() {
+        return notebookRepository.findAll();
+    }
+
+    @Override
+    public void saveNotebook(Notebook notebook) {
+        notebookRepository.save(notebook);
+    }
+
+    @Override
+    public Notebook getNotebookById(Long id) throws NotebookNotFoundException {
+        Optional<Notebook> notebookOptional = notebookRepository.findById(id);
+
+        if (notebookOptional.isEmpty()) {
+            throw new NotebookNotFoundException();
+        }
+
+        return notebookOptional.get();
+    }
 }
